c8009369a3a994067fd44125bfd05eec1e33f1f5
danjstern <danjstern@users.noreply.github.com>
2020-08-18 14:51:52 -0700

    Update localStorageTest.js

--- localStorageTest.js
+++ localStorageTest.js
@@ -1,7 +1,9 @@
-try {
+function testLocalStorage() {
+  try {
 const testLocalStorage = window.localStorage;
 console.log(testLocalStorage);
 }
 catch(err) {
   console.log("No Access to Local Storage");
+}
 }
